0511fe79e1efc4b09a67d0b98bc7c5e4438de1dd
Nobuaki-Shumi <159235587+Nobuaki-Shumi@users.noreply.github.com>
2024-03-14 15:05:12 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+//ハンバーガーメニュー
+
+$(function (){
+    $('.humburger-btn').on('click',function () {
+        $('.humburger-menu').toggleClass('show')
+    })
+})
+
+//フェードイン
+$(function (){
+    $('.skill-text,.service-item').on('inview',function(){
+        $(this).addClass('fadeIn')
+    })
+})
+
+//画像のスライダー
+$(function (){
+    $('.slider1').slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 3000,
+        cssEase: "Linear",
+        slidesToShow: 5,
+        swipe: false,
+        arrows: false,
+        responsive: [
+            {
+            breakpoint: 599,
+            settings: {
+                slidesToShow: 3,
+            }
+        }
+        ]
+    })
+})
+
+$(function (){
+    $('.slider2').slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 3000,
+        cssEase: "Linear",
+        slidesToShow: 5,
+        swipe: false,
+        arrows: false,
+        rtl: true,
+        responsive: [
+            {
+            breakpoint: 599,
+            settings: {
+                slidesToShow: 3,
+            }
+        }
+        ]
+    })
+})
